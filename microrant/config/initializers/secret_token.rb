d905c0c64ce94dd605dbc162bee88e86caab24b0
Audrey Eschright <spinnerin@gmail.com>
2011-08-17 09:50:24 -0700

    Checking in the base app for our Rails demo (microrant).

---
+++ microrant/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microrant::Application.config.secret_token = '59c4ad90d6450a0cb2fe42b10aba59a30c9cf8ee4266035b4f7789fde05d96a289c17b7a114cd833a7fa833d88fb4e6c39a67b7393e50e032c4d9b50b48a54f5'
